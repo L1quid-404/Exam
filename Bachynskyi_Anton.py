74d18a958d76f454ffef17218000423bc4494644
Anton <bachynskyi.anton@gmail.com>
2025-12-23 13:28:50 +0200

    Added colors and fixed translation logic

--- Bachynskyi_Anton.py
+++ Bachynskyi_Anton.py
@@ -1,58 +1,64 @@
 # .bachynskyi_exam
 import sys
+try:
+    import cgi
+except ImportError:
+    import legacy_cgi as cgi
+
 from googletrans import Translator
 
-lang = "Japanese"
+lang = "Ukrainian"
 
-BLUE = "\033[34m"
 RED = "\033[31m"
+BLUE = "\033[34m"
 RESET = "\033[0m"
 
 translator = Translator()
 
-def translate_msg(text, target_lang):
+def get_text(text, target_lang):
+    """Перекладає текст, якщо обрано не українську мову"""
+    if target_lang.lower() in ["ukrainian", "uk"]:
+        return text
     try:
         return translator.translate(text, src='uk', dest=target_lang).text
-    except Exception:
+    except:
         return text
 
 def main():
-    prompt_txt = "Введіть довжини сторін трикутника a, b, c: "
-    exists_txt = "Трикутник з сторонами {} існує."
-    iso_txt = " є рівнобедреним"
-    reason_txt = " так як дві його сторони дорівнюють "
+    prompt = get_text("Введіть довжини сторін трикутника a, b, c: ", lang)
+    exists_msg = get_text("Трикутник з сторонами {} існує.", lang)
+    iso_part = get_text("є рівнобедреним", lang)
+    reason_part = get_text("так як дві його сторони дорівнюють", lang)
+    intro_part = get_text("Трикутник з сторонами", lang)
 
-    p_label = translate_msg(prompt_txt, lang)
-    
-    user_input = input(p_label)
+    user_input = input(prompt)
     try:
         sides = [float(x) for x in user_input.split()]
         if len(sides) != 3: raise ValueError
         a, b, c = sides
     except ValueError:
-        print("Error: Input 3 numbers.")
+        print("Помилка вводу!")
         return
 
     def f(n): return str(int(n) if n.is_integer() else n)
 
     if (a + b > c) and (a + c > b) and (b + c > a):
-        translated_exists = translate_msg(exists_txt, lang)
         sides_blue = f"{BLUE}{f(a)},{f(b)},{f(c)}{RESET}"
-        print(translated_exists.format(sides_blue))
+        print(exists_msg.format(sides_blue))
 
-        iso_data = None
-        if a == b: iso_data = a
-        elif a == c: iso_data = a
-        elif b == c: iso_data = b
+        equal_side = None
+        if a == b: equal_side = a
+        elif a == c: equal_side = a
+        elif b == c: equal_side = b
 
-        if iso_data:
-            res_iso = translate_msg(iso_txt, lang)
-            res_reason = translate_msg(reason_txt, lang)
-            
+        if equal_side is not None:
             sides_red = f"{RED}{f(a)}, {f(b)}, {f(c)}{RESET}"
-            print(f"Triangle {sides_red}{BLUE}{res_iso}{RESET}{res_reason}{RED}{f(iso_data)}{RESET}.")
+            blue_iso = f"{BLUE}{iso_part}{RESET}"
+            red_value = f"{RED}{f(equal_side)}{RESET}"
+            
+            print(f"{intro_part} {sides_red} {blue_iso} {reason_part} {red_value}.")
     else:
-        print(translate_msg("Трикутник не існує.", lang))
+        print(get_text("Трикутник не існує.", lang))
 
 if __name__ == "__main__":
     main()
